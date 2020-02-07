a5b0094fde819a33ea28f1c89780888a50a13209
Annie Deng <anniedeng@live.com>
2020-02-07 12:21:41 -0800

    added p5.js and sketch to portal page

--- des157-wi20/js/script.js
+++ des157-wi20/js/script.js
@@ -1,18 +1,21 @@
 function setup() {
-    var myCanvas = createCanvas(800, 250);
+    var myCanvas = createCanvas(1000, 250);
     frameRate(8);
-    noStroke();
-    background(random(255), random(255), random(255));
+    stroke(255);
+    background("#e5edf1");
     myCanvas.parent(mySketch);
   }
   
   function draw() {
     if(mouseIsPressed) {
-      fill(0);
-    } else {
-      fill(random(255), random(255), random(255), random(255));
+      fill(255);
     }
-      var circSize = random(200);
-    ellipse(mouseX, mouseY, circSize, circSize);
+
+    else {
+    strokeWeight(5);
+    fill("#d6daef");
+    }
+      var circSize = random(100);
+    rect(mouseX, mouseY, circSize, circSize);
   }
   
