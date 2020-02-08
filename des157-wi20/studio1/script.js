86d63693ff30239014cf220ed7383c7bc2c0c4e4
yeuji <59749470+yeuji@users.noreply.github.com>
2020-02-08 02:42:51 -0800

    studio1

--- des157-wi20/studio1/script.js
+++ des157-wi20/studio1/script.js
@@ -1,61 +1,26 @@
-const myForm = document.querySelector("form");
-const noun1 = document.getElementById("noun1").value;
-const noun2 = document.getElementById("noun2").value;
-const adjective = document.getElementById("adjective").value;
-const verb = document.getElementById("verb").value;
-const adverb = document.getElementById("adverb").value;
+(function(){
+  "use strict";
 
+  document.getElementById("myForm").addEventListener("submit", function(event){
+      event.preventDefault();
 
+      const formData = document.querySelectorAll("input[type=text]");
+      const words = [];
 
-myForm.addEventListener("submit", function(e){
-    e.preventDefault();
-    
+      for(var i=0; i<formData.length; i++){
+          words.push(formData[i].value);
+      }
 
-});
+      makeMadLib(words);
+  });
 
- // listen for the submission of the form
- document.getElementById("myform").addEventListener("submit", function(event){
-    // prevent the default behavior
-    event.preventDefault();
+  function makeMadLib(theWords){
+      const madlib = `My ${theWords[0]}. When we first met, you looked so ${theWords[1]} and ${theWords[2]}. I couldn't believe my eyes. You smelled like a ${theWords[3]}. You walked so ${theWords[4]}. I couldn't wait to ${theWords[5]} you.`;
 
-    // get all the text fields from the form
-    var formData = document.querySelectorAll("input[type=text]");
-    // create an array to hold values from the form
-    var words = [];
+      const mlContainer = document.getElementById('madlib');
 
-    //loop through the fields and add the words to the array, one at a time.
-    for( var i=0; i<formData.length; i++){
-        words.push(formData[i].value);    
-    }
-    //You can see the array in the console, if you want...
-    console.log(words);
+      mlContainer.innerHTML = `<p>${madlib}</p>`;
 
-    //run a function to put the words into the madlib... Function below...
-    //Pass the array of words the user typed into the function
-    makeMadLib(words);
-
-});
-
-//This function takes an array input, then constructs the madlib, peppering
-//in the user's words where necessary.
-function makeMadLib(theWords){
-    var madlib = `Lorem ipsum dolor ${theWords[0]} sit amet consectetur adipisicing elit. Doloremque ${theWords[1]} iste  ratione cum amet! Vitae ${theWords[2]} earum dolorem nesciunt nam eos sunt, itaque alias, minima ${theWords[3]} explicabo repellendus animi sapiente nobis inventore doloremque? `;
-
-    //Get the madlib container
-    var mlContainer = document.getElementById('madlib');
-
-    //Change the guts of the container to your madlib
-    mlContainer.innerHTML = `<p>${madlib}</p>`;
-
-    //change the class on the madlib container so you can see it.
-    mlContainer.setAttribute("class", "visible");
-}
-
-/*******************************
-1. Add interesting graphics/fun madlib content/theme
-2. Change styling (add fonts from google fonts, colors, etc)
-3. Change the way the madlib appears (add a css animation?)
-4. Adjust JS to suit your madlib
-5. Refactor JS (put in external script file, put in enclosure, 
-change var to const or let, etc.)
-*********************************/ 
+      mlContainer.setAttribute("class", "visible");
+  }
+}());
