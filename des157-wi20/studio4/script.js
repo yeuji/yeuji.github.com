d10beaaf23eb1ade5f5c33d7f7decb71c15d25e1
Annie Deng <anniedeng@live.com>
2020-03-06 16:01:37 -0800

    studio4

--- des157-wi20/studio4/script.js
+++ des157-wi20/studio4/script.js
@@ -1,28 +1,35 @@
 (function(){
   "use strict";
 
-  const close = document.getElementById("close");
-
-  close.addEventListener("click", function(){
-    container.style.display = "none";
-  });
-
   document.getElementById("myForm").addEventListener("submit", function(event){
 
       event.preventDefault();
-      const formData = document.querySelectorAll("input[type=text]");
-      const words = [];
+      const userName = document.querySelector('#name').value;
+      const formData = document.querySelectorAll("input[type=radio]");
+      let amount;
       for(var i=0; i<formData.length; i++){
-          words.push(formData[i].value);
+          if(formData[i].checked == true){
+            amount = formData[i].value;
+          }
       }
 
-      makeMadLib(words);
+      if(!amount){
+        amount = document.querySelector("#other").value;
+      }
+
+      makeMadLib(userName, amount);
   });
 
-  function makeMadLib(theWords){
-      const madlib = `${theWords[0]}! Your donation of $${theWords[2]} will be used to help clean plastic trash out of the ocean. Have a wonderful day and stay sustainable!`;
+  function makeMadLib(userName, amount){
+      const close = document.getElementById("close");
+      const madlib = `${userName}! Your donation of $${amount} will be used to help clean plastic trash out of the ocean. Have a wonderful day and stay sustainable!`;
       const mlContainer = document.getElementById('madlib');
-      mlContainer.innerHTML = `<h3>Thank you!</h3><p>${madlib}</p>`;
+      const popupContainer = document.getElementById('popup');
+      popupContainer.innerHTML = `<h3>Thank you!</h3><p>${madlib}</p>`;
       mlContainer.setAttribute("class", "visible");
+      close.addEventListener("click", function(){
+        mlContainer.style.display = "none";
+      });
   }
+
 }());
