bde12f3a788871d2974afe86816e454c4e37e16c
yeuji <59749470+yeuji@users.noreply.github.com>
2020-03-13 19:17:36 -0700

    final

--- des157-wi20/process/development/final/calculator.js
+++ des157-wi20/process/development/final/calculator.js
@@ -1,17 +1,17 @@
 (function(){
   "use strict";
 
-  var myForm = document.querySelector("form");
+  const myForm = document.querySelector("form");
 
   myForm.addEventListener("submit", function(e){
     e.preventDefault();
-    var myForm = document.querySelector("form");
-    var myAnswer = document.getElementById("answer");
-    var paragraph = document.getElementById("paragraph");
-    var co2 = document.getElementById("co2");
+    const myForm = document.querySelector("form");
+    const myAnswer = document.getElementById("answer");
+    const paragraph = document.getElementById("paragraph");
+    const co2 = document.getElementById("co2");
     co2.style.color = "green";
-    var bottles = parseFloat(document.querySelector("input").value);
-    var emissions = 3.76 * bottles;
+    const bottles = parseFloat(document.querySelector("input").value);
+    let emissions = 3.76 * bottles;
     emissions = emissions.toFixed(1);
 
     if(bottles){
